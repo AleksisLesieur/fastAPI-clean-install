c41ab25dc67594e72fec5cee108d866120d0b059
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-12-27 02:14:01 +0200

    disconnecting 3rd player

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 
+
 app = FastAPI()
 
 app.add_middleware(
@@ -32,16 +33,30 @@ class BoxData(BaseModel):
     cell_1_8: Optional[str] = None
     cell_1_9: Optional[str] = None
 
+
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
+        self.connection_count = 0
 
     async def connect(self, websocket: WebSocket):
+        # if self.connection_count <= 2:
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if self.connection_count < 2:
+            self.active_connections.append(websocket)
+            self.connection_count += 1
+
+            if self.connection_count == 1:
+                await self.broadcast("waiting_for_player")
+
+        else:
+            await websocket.send_text('full_room')
+            await websocket.close(code=1000)
+
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)   
+        self.active_connections.remove(websocket)
+        self.connection_count = 0
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -53,7 +68,6 @@ class ConnectionManager:
     async def reset(self):
         self.active_connections.clear()
 
-
 manager = ConnectionManager()
 
 class GameState:
@@ -61,16 +75,12 @@ class GameState:
         self.board = [None] * 9
         self.current_player = 'X'
         self.last_player = None
-        self.first_player = False
-        self.second_player = False
-        self.player1_id = None
-        self.player2_id = None
+        self.temporary_id = None
+        # self.player2_id = None
 
     def set_player_id(self, client_id):
-        if not self.player1_id:
-            self.player1_id = client_id
-        elif not self.player2_id and self.player1_id != client_id:
-            self.player2_id = client_id
+        if not self.temporary_id:
+            self.temporary_id = client_id
 
     def make_move(self, index: int, client_id):
         if self.last_player == client_id:
@@ -79,14 +89,8 @@ class GameState:
         if self.board[index] is None:
             self.board[index] = self.current_player
             self.current_player = 'O' if self.current_player == 'X' else 'X'
-            self.first_player = self.player1_id > self.player2_id
-            self.second_player = self.player1_id < self.player2_id
             self.last_player = client_id
             return True
-        
-    # def switch_player(self, first_player, second_player):
-    #     first_player = not first_player
-    #     second_player = not second_player 
 
         return False
 
@@ -100,30 +104,34 @@ game_state = GameState()
 async def read_root(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
 
+
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
-    print(websocket)
     try:
         while True:
             data = await websocket.receive_text()
             array_data = json.loads(data)
-                # add game logic here
+            # add game logic here
             print(array_data)
             index = int(array_data[0])
             received_client_id = int(array_data[1])
             game_state.set_player_id(received_client_id)
+
             if game_state.make_move(index, client_id):
-                await manager.broadcast(json.dumps({"board": game_state.board, "currentPlayer": game_state.current_player, "firstPlayer": game_state.first_player, "secondPlayer": game_state.second_player}))
+                await manager.broadcast(json.dumps({
+                    "board": game_state.board,
+                    "currentPlayer": game_state.current_player,
+                    "playerId": received_client_id,
+                    "connectionCount": manager.connection_count,
+                    "playerIndex": index,
+                }))
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.reset()
-        await manager.broadcast(json.dumps({"type": "disconnection", "message": f"Player {client_id} disconnected"}))
         game_state.reset()
-       
+        await manager.broadcast(json.dumps({"type": "disconnection", "message": f"Player {client_id} disconnected"}))
 
-
-
-
-
+# async def update_modal():
+#     await manager.broadcast(json.dumps({"type": 'playerCount', "count": manager.connection_count}))
 
--- static/script.js
+++ static/script.js
@@ -1,68 +1,97 @@
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll(".box");
 
-let currentPlayerFront = null
+let currentPlayerFront = null;
 
-let currentBox = null
+let currentBox = null;
 
-// const modal = document.getElementById("modal");
+const modal = document.querySelector(".modal");
 
-const firstPlayerModal = document.querySelector('.firstPlayer')
+const modalContent = document.querySelector('.modal-content');
 
-const secondPlayerModal = document.querySelector(".secondPlayer");
+const modalText = document.querySelector(".modal-text");
 
-let clientId = Date.now() - 1503023345436
+const dots = document.querySelector(".dots")
+
+
+// Create a style element
+const style = document.createElement('style');
+
+// Add your CSS rules to override the modal-content::after properties
+style.innerHTML = `
+.modal-content::after {
+    content: none;
+    animation: none;
+}
+
+.modal-text .dots {
+    display: none;
+}`;
+
+let showModal = false;
+
+let clientId = Date.now() - 1_703_023_345_436;
 
 let receivedData;
 
-const socket = new WebSocket(`ws://localhost:8000/ws/${clientId}`)
+const socket = new WebSocket(`ws://localhost:8000/ws/${clientId}`);
 
-socket.onopen = function(event) {
-    console.log('WebSocket connection established');
-    console.log(event)
-    firstPlayerModal.style.display = "none";
-    secondPlayerModal.style.display = "none";
+socket.onopen = function (event) {
+  console.log("WebSocket connection established");
+  console.log(event);
+  modal.style.display = "none";
+};
+
+socket.onclose = function (e) {
+  console.log("WebSocket connection closed", e);
+  // modal.style.display = "block";
 };
 
 socket.onmessage = function (event) {
-    receivedData = JSON.parse(event.data)
-    console.log(receivedData)
-    console.log(clientId)
-    // if (receivedData.firstPlayer) {
-    //   // Show modal for disconnection
-    //     firstPlayerModal.style.display = "block";
-    //     secondPlayerModal.style.display = "none";
-    // }
-    // else if (receivedData.secondPlayer) {
-    //     firstPlayerModal.style.display = "none";
-    //     secondPlayerModal.style.display = "block";
-    // }
+  console.log("on message event");
+  console.log(event);
 
-    for (let i = 0; i < 9; i++) {
-        boxes[i].textContent = receivedData['board'][i]
+  if (event.data === "waiting_for_player") {
+    modal.style.display = "block";
+    modalText.textContent = "Waiting for the other player to connect";
+    return;
+  } else if (event.data === "full_room") {
+    modal.style.display = "block";
+    modalText.textContent = "Sorry, the room is currently full, please try again later!";
+    document.head.appendChild(style);
+    return;
+  }
+
+  receivedData = JSON.parse(event.data);
+  let temporaryId = clientId;
+  let receivedId = receivedData.playerId;
+  console.log(receivedData);
+  if (receivedId === temporaryId) {
+    modal.style.display = "block";
+    modalText.textContent = "Waiting for the other player turn";
+  } else {
+    modal.style.display = "none";
+  }
+  for (let i = 0; i < 9; i++) {
+    boxes[i].textContent = receivedData["board"][i];
+  }
+  if (receivedData.type === "playerCount") {
+    console.log(receivedData.count);
+  }
+  currentPlayerFront = receivedData["currentPlayer"];
+};
+
+socket.onerror = function (err) {
+  console.error("WebSocket error", err);
+  socket.close();
+};
+
+console.log(boxes[0]);
+console.log("boxes above this");
+
+boxes.forEach(function (element, index) {
+  element.addEventListener("click", function (boardData) {
+    if (boxes[index].textContent === "") {
+      socket.send(JSON.stringify([index.toString(), clientId.toString()]));
     }
-    currentPlayerFront = receivedData['currentPlayer']
-};
-
-socket.onclose = function(e) {
-    console.log('WebSocket connection closed', e);
-    // modal.style.display = "block";
-};
-
-socket.onerror = function(err) {
-    console.error('WebSocket error', err);
-    socket.close();
-};
-
-console.log(boxes[0])
-console.log('boxes above this')
-
-boxes.forEach(function(element, index ){
-    element.addEventListener('click', function(boardData) {
-        if (boxes[index].textContent === '') {
-            console.log("sending")
-            // socket.send(index.toString())
-            socket.send(JSON.stringify([index.toString(), clientId.toString()]));
-            console.log(boxes[index].textContent)
-        }
-    })
-})
+  });
+});
